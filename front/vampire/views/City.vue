7ea36bd6312ee847cdae95fa6ae3f6ba3a11fd53
jgcohen <75163282+jgcohen@users.noreply.github.com>
2021-07-25 18:56:01 +0200

    comments for later

--- front/vampire/views/City.vue
+++ front/vampire/views/City.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-        
+        <!-- ICI je veux afficher les differentes City -->
     </div>
 </template>
 
 <script>
 export default {
-    
+    // Ici je veux acceder aux fonctions de mon API
 }
 </script>
